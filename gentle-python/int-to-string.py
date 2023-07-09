1da5d7d42d0e61891844702bd3c057f96605143e
Jakub_szymborski <kuba.szymborski@gmail.com>
2023-07-09 13:26:12 +0200

    int to string upload

--- gentle-python/int-to-string.py
+++ gentle-python/int-to-string.py
@@ -12,13 +12,50 @@ defeat the purpose of this exercise.
 """
 
 
-def convertIntToStr(integer):
-    pass
+def convertIntToStr(integerNum):
+    stringNum = ''  
+    sign = ''
+    numDict = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}    # numbers dict to not use str()
+
+    if integerNum == 0:
+        return '0'
+    
+    if integerNum < 0:
+        sign = '-'
+        integerNum = integerNum*(-1)
+    
+    while int(integerNum/10) != 0:  # loop will break at last digit, do_while would be nice 
+        digit = integerNum % 10              # - rest from division by 10 is the last digit  
+        integerNum =  int(integerNum / 10)   # removing last digit      
+        stringNum = numDict[digit] + stringNum        
+    
+    digit = integerNum % 10                 # one more time for last digit  
+    stringNum = numDict[digit] + stringNum          
+
+    return sign + stringNum
 
 
+def convertIntToStr2(integerNum):
+    stringNum = ''  
+    sign = ''
+    numDict = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}    # numbers dict to not use str()
+
+    if integerNum == 0:
+        return '0'
+    
+    if integerNum < 0:
+        sign = '-'
+        integerNum = integerNum*(-1)
+    
+    while integerNum != 0:   
+        digit = integerNum % 10              # - rest from division by 10 is the last digit  
+        integerNum =  int(integerNum / 10)   # removing last digit      
+        stringNum = numDict[digit] + stringNum        
+    
+    return sign + stringNum
 
 
 
 
 for i in range(-10000, 10000):
-    assert convertIntToStr(i) == str(i)
+    assert convertIntToStr2(i) == str(i)
